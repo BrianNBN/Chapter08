219b7af216137fe79eff2146460c48524ad35b18
Brian Nyagwansa <bnyagwansa2@gmail.com>
2020-09-18 11:36:15 +0300

    first commit

---
+++ Strict.js
@@ -0,0 +1,9 @@
+//JavaScript can be made a little stricter by enabling strict mode
+function canYouSpotTheProblem (){
+    'use strict';
+    for (counter = 0; counter < 10; counter++){
+        console.log ("Happy Hppy");
+    }
+}
+
+canYouSpotTheProblem();
